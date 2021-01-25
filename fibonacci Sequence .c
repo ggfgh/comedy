93a843f77e59fc504a2138d3e71d01d60dc7b02c
yaowen <71026994+ggfgh@users.noreply.github.com>
2021-01-25 21:32:16 +0800

    Update fibonacci Sequence .c

--- fibonacci Sequence .c
+++ fibonacci Sequence .c
@@ -2,7 +2,21 @@
 
 int main()
 {
+/*斐波那契数列F(0)=0，F(1)=1, F(n)=F(n - 1)+F(n - 2)（n ≥ 2，n ∈ N*）*/
 
+        long a = 1, b = 1, c, i;
+
+        for (i = 3; i <= 24; i++)
+        {
+                c = a + b;
+                a = b;
+                b = c;
+        }
+
+        printf("两年后，总共有%ld只兔子！\n", c);
+
+        return 0;
+*/
   long a = 1, b = 1, c, i;
 
         for (i = 3; i <= 24; i++)
