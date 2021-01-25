258293b9b5b4cd31fbcd9c83f7219217534a16bb
yaowen <71026994+ggfgh@users.noreply.github.com>
2021-01-26 02:17:07 +0800

    Update bubble Sort .cs

--- bubble Sort .cs
+++ bubble Sort .cs
@@ -4,23 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace 冒泡排序
+namespace _冒泡排序_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /* 第一次交换：8 7 6 5 4 3 2 1 0 9   9交换了9次 9 = nums.Length - 1 -i
-             * 第二次交换：7 6 5 4 3 2 2 0 8 9   8交换了8次 8 = nums.Length - 1 - i
-             * 第三次交换：6                               
-             * 第三次交换  5
-             * 第三次交换  4
-             * 第三次交换  3
-             * 第三次交换  2
-             * 第三次交换  1
-             * 第九次交换  0
-             
-             */
+           /* 第一次交换：8 7 6 5 4 3 2 1 0 9   9交换了9次 9 = nums.Length - 1 -i
+             第二次交换：7 6 5 4 3 2 2 0 8 9   8交换了8次 8 = nums.Length - 1 - i  */
             int[] nums = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
             for (int i = 0; i < nums.Length - 1; i++)
             {
